9d0f359aa076d6506738f0db28213f95cdbbf54b
UDIT GAURAV <35391335+uditgaurav@users.noreply.github.com>
2020-09-03 23:57:54 +0530

    Create multiarch-build.sh

---
+++ litmus/multiarch-build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+package=$1
+if [[ -z "$package" ]]; then
+  echo "usage: $0 <package-name>"
+  exit 1
+fi
+package_split=(${package//\// })
+package_name=${package_split[-1]}
+
+platforms=("linux/amd64" "linux/arm64")
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    output_name=chaos-runner$GOARCH
+
+    env GOOS=$GOOS GOARCH=$GOARCH sh $package 
+   
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+done
